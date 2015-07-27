482ffb070e8f366667bf95dede4339badee719c3
Mario Cecchi <macecchi@gmail.com>
2015-07-26 21:03:27 -0300

    Updates config parameters and logs

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,15 +1,14 @@
 var iTunes = {};
 iTunes.state = "playing";
 
-iTunes.iTunesServer = localStorage.getItem("server");
-iTunes.configureUrl = "http://spangborn.github.io/pebble-plex-remote/index.html";
+iTunes.server = localStorage.getItem("server");
+iTunes.configureUrl = "https://macecchi.github.io/pebble-itunes-remote/index.html";
 
-// This won't work without Plexpass, BAH.
 iTunes.getPlaying = function (command) {
 	var req1 = new XMLHttpRequest();
 	req1.timeout = 2000;
 
-	req1.open("GET", "http://" + iTunes.iTunesServer + ":8080/status", true);
+	req1.open("GET", "http://" + iTunes.server + ":8080/status", true);
 	req1.onreadystatechange = function (e) {
 		if (req1.status != 200) {
 			console.log("Error communicating to server. Response code was " + req1.status);
@@ -38,27 +37,29 @@ iTunes.doCommand = function (action) {
 };
 
 iTunes.sendCommand = function  (command) {
+	console.log("Sending command to http://" + iTunes.server + ":8080/" + command);
+
 	var req = new XMLHttpRequest();
 	req.timeout = 2000;
-	req.open("GET", "http://" + iTunes.iTunesServer + ":8080/" + command, true);
+	req.open("GET", "http://" + iTunes.server + ":8080/" + command, true);
 	req.onreadystatechange = function (e) {
 		if (req.readyState === 4) {  
-			if (req.status === 200) {  
-				console.log("Request sent: " + command);  
-		  	}
-		  	else {
-		  		console.log("Request failed with status " + req.status);
-		  	}
+			if (req.status === 200) {
+				console.log("Response OK for command: " + command);
+			}
+			else {
+				console.log("Request failed with status " + req.status);
+			}
 		} 
 	};
 	req.onerror = function (e) {
 		setTimeout(function(){
-			Pebble.showSimpleNotificationOnPebble("iTunes", "Unable to reach iTunes.");	
-		}, 1000);
+			Pebble.showSimpleNotificationOnPebble("iTunes Remote", "Unable to reach iTunes. Check the settings and status of iTunes API.");	
+		}, 3000);
 	};
 	req.send(null);
-	
 };
+
 Pebble.addEventListener("ready", function(e) {
 	console.log("iTunes Remote is go.");
 });
@@ -69,27 +70,21 @@ Pebble.addEventListener("appmessage", function(e) {
 		iTunes.doCommand(e.payload.action);
 	}
 });
+
 Pebble.addEventListener("showConfiguration", function(e) {
 	Pebble.openURL(iTunes.configureUrl);
 });
+
 Pebble.addEventListener("webviewclosed",
-  function(e) {
+	function(e) {
+		if (e.response) {
+			var configuration = JSON.parse(e.response);
+			console.log("Configuration window returned: " + JSON.stringify(configuration));
 
-    if (e.response) {
+			console.log("iTunes Server: " + configuration.server);
+			localStorage.setItem("server", configuration.server);  
 
-    	var configuration = JSON.parse(e.response);
-    	console.log("Configuration window returned: " + JSON.stringify(configuration));
-    	
-    	console.log("iTunes Server: " + configuration.iTunesServer);
-    	console.log("iTunes Client: " + configuration.iTunesClient);
-    	console.log("iTunes Token: " + configuration.iTunesToken);
-    	localStorage.setItem("iTunesServer", configuration.iTunesServer);  
-		localStorage.setItem("iTunesClient", configuration.iTunesClient); 
-		localStorage.setItem("iTunesToken", configuration.iTunesToken); 
-
-		iTunes.iTunesToken = configuration.iTunesToken;
-		iTunes.iTunesServer = configuration.iTunesServer;
-		iTunes.iTunesClient = configuration.iTunesClient;
+			iTunes.server = configuration.server;
+		}
 	}
-  }
-);
+	);
