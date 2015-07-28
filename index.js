918b88eb5a679af40cad83e2b744eb763cb67098
Mario Cecchi <macecchi@gmail.com>
2015-07-28 00:09:20 -0300

    Adds icon to status bar instead of text

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 var gui = require('nw.gui');
+var iTunesAPI = require('itunes-control-api');
+iTunesAPI.start(8080);
+
+var localIP;
+
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  localIP = add;
+	menu.insert(new gui.MenuItem({
+	    label: 'Server running on ' + localIP,
+	    enabled: false
+	}), 0);
+})
 
 var tray = new gui.Tray({
-    title: 'Remote'
+    icon: 'bar_icon.png'
 });
+
 var menu = new gui.Menu();
-menu.append(new gui.MenuItem({
-    type: 'checkbox',
-    label: 'Always-on-top',
-    click: function () { }
-  }));
 tray.menu = menu;
 
-console.log('done');
+
+menu.append(new gui.MenuItem({
+    type: 'separator'
+}));
+
+menu.append(new gui.MenuItem({
+    label: 'Exit',
+    click: function() { 
+    	console.log('Clicked exit menu');
+		gui.App.quit();
+    }
+}));
