b3d749cf35ace72e58c675d3630d117220125ac8
Mario Cecchi <macecchi@gmail.com>
2015-08-02 01:43:38 -0300

    Spotify support

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var gui = require('nw.gui');
-var iTunesAPI = require('itunes-control-api');
-iTunesAPI.start(8080);
+var express = require('express');
+var app = express();
+var iTunes = require('local-itunes');
+var spotify = require('spotify-node-applescript');
 
 var localIP;
+var activePlayer = 'itunes';
+
+
+// GUI
 
 require('dns').lookup(require('os').hostname(), function (err, add, fam) {
   localIP = add;
@@ -19,6 +25,32 @@ var tray = new gui.Tray({
 var menu = new gui.Menu();
 tray.menu = menu;
 
+menu.append(new gui.MenuItem({
+    type: 'separator'
+}));
+
+var iTunesMenuItem = new gui.MenuItem({
+    label: 'Control iTunes',
+    type: 'checkbox',
+    checked: true,
+    click: function() { 
+        console.log('Switching to iTunes control');
+        activePlayer = 'itunes';
+        spotifyMenuItem.checked = false;
+    }
+});
+menu.append(iTunesMenuItem);
+
+var spotifyMenuItem = new gui.MenuItem({
+    label: 'Control Spotify',
+    type: 'checkbox',
+    click: function() { 
+        console.log('Switching to Spotify control');
+        activePlayer = 'spotify';
+        iTunesMenuItem.checked = false;
+    }
+});
+menu.append(spotifyMenuItem);
 
 menu.append(new gui.MenuItem({
     type: 'separator'
@@ -31,3 +63,51 @@ menu.append(new gui.MenuItem({
 		gui.App.quit();
     }
 }));
+
+// Playback control
+
+app.get('/playpause', function(req, res){
+    console.log("[GET] /playpause");
+    if (activePlayer == 'itunes') {
+        iTunes.playpause();
+        iTunes.playerState(function(error, state){
+          // res.json({ state: state });
+        });
+    }
+    else if (activePlayer == 'spotify') {
+        spotify.playPause(function(error, state){
+            // res.json({ state: state.state });
+        });
+    }
+    res.end();
+});
+
+app.get('/previous', function(req, res){
+    console.log("[GET] /previous");
+    if (activePlayer == 'itunes') {
+        iTunes.previous();
+    }
+    else if (activePlayer == 'spotify') {
+        spotify.previous(function(error, state){
+            // res.json({ state: state.state });
+        });
+    }
+    res.end();
+});
+
+app.get('/next', function(req, res){
+    console.log("[GET] /next");
+    if (activePlayer == 'itunes') {
+        iTunes.next();
+    }
+    else if (activePlayer == 'spotify') {
+        spotify.next(function(error, state){
+            // res.json({ state: state.state });
+        });
+    }
+    res.end();
+});
+
+var port = 8080;
+var server = app.listen(port);
+console.log('Pebble iTunes Remote Server started on port ' + port);
